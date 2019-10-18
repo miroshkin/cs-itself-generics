7000d0f7fb4478338200f5042ab3be024193a11e
Anton Miroshkin <anton.miroshkin@gmail.com>
2019-10-18 14:30:41 +0300

    Added project

---
+++ CS.Itself.Generics/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS.Itself.Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CustomTypeInfo<string>.GetTypeInfo());
+            Console.WriteLine(CustomTypeInfo<DateTime>.GetTypeInfo());
+            Console.WriteLine(CustomTypeInfo<Array>.GetTypeInfo());
+        }
+    }
+
+    static class CustomTypeInfo <T>
+    {
+        public static string GetTypeInfo()
+        {
+            return typeof(T).ToString();
+        }
+    }
+}
